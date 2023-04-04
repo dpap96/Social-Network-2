83ed83c76080068f53211402336a8312a5e07092
Drpapadopoulou <82087214+dpap96@users.noreply.github.com>
2023-04-05 01:47:28 +0300

    Add files via upload

---
+++ src/ClosedGroup.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ClosedGroup extends General_Groups {
+	
+
+	public ClosedGroup(String name, String description) {
+		nameGroup=name;
+		description=description;
+		
+	}
+	
+	public boolean checkingtoBeinClosedGroup(User x) {
+		if( x.friendList(x))
+		
+	}
+	 
+	
+	
+	
+	
+	
+	
+}
